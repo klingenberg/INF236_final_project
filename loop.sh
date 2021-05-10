7d38ae84608c76f28e31bf2b830a8838127dc4b5
Luca Klingenberg <luca.klingenberg@tum.de>
2021-05-10 11:39:54 +0200

    fix loop

--- loop.sh
+++ loop.sh
@@ -7,23 +7,18 @@ counter2=256
 
 while [ $counter2 -le 4096 ]
 do
-    counter=2
-    while [ $counter -le 256 ]
+    for i in "${threads[@]}"
     do
-        for i in "${threads[@]}"
-        do
-            export OMP_SET_NUM_THREADS=$i
-            ex1=1
-            ex2=1
-            if [ $i -gt 1 ]
-            then
-                ex1=0
-                ex2=0
-            fi
-            echo $counter2 $counter $i $ex1 $ex2 1 1 1
-            ./driver $counter2 $counter $ex1 $ex2 1 1 1
-        done
-        counter=$(( $counter * 2 ))
+        export OMP_SET_NUM_THREADS=$i
+        ex1=1
+        ex2=1
+        if [ $i -gt 1 ]
+        then
+            ex1=0
+            ex2=0
+        fi
+        echo $counter2 $i $ex1 $ex2 1 1 1
+        ./driver $counter2 $ex1 $ex2 1 1 1
     done
     counter2=$(( $counter2 * 2 ))
 done
